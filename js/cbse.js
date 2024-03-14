8fcb250aa1bbf2ebc8d583d8169a7ccf84eebd35
Aniccha <shubhwaghmare27@gmail.com>
2024-03-14 22:00:10 +0530

    new subjects added in cbse

---
+++ js/cbse.js
@@ -0,0 +1,22 @@
+let classes =[
+    {
+        name:"class 8th",
+        img:"../../src/cbse/maths8th.jpg"
+    },
+    {
+        name:"class 9th",
+        img:"../../src/cbse/maths9th.jpg"
+    },
+    {
+        name:"class 10th",
+        img:"../../src/cbse/maths10th.jpg"
+    },
+    {
+        name:"class 11th",
+        img:"../../src/cbse/maths11th.jpg"
+    },
+    {
+        name:"class 12th",
+        img:"../../src/cbse/maths12th.jpg"
+    },
+]
